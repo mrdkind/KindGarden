e127a431447601114855f7c78ce247eb08f618d9
mrdkind <mrkind9@gmail.com>
2025-03-17 22:02:28 +0330

    Quartz sync: Mar 17, 2025, 10:02 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,11 +5,25 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'eledah/quartz_blog',
+        // from data-repo-id
+        repoId: 'R_kgDOLxbW_g',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOLxbW_s4ChRbJ',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      // "آپارات": "https://www.aparat.com/Crystalline",
+      // "گیت‌هاب": "https://github.com/eledah",
     },
   }),
 }
@@ -23,41 +37,45 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
+    // Component.DesktopOnly(Component.Sidenotes()),
     Component.DesktopOnly(Component.Graph()),
     Component.DesktopOnly(Component.Backlinks()),
- 
+
+    Component.MobileOnly(Component.PageTitle()),
+    Component.MobileOnly(Component.Darkmode()),
+    // Component.MobileOnly(Component.Search()),
+  ],
+  right: [
+    Component.DesktopOnly(Component.PageTitle()),
+    Component.DesktopOnly(Component.Darkmode()),
+    Component.DesktopOnly(Component.Search()),
+    // Component.DesktopOnly(Component.Explorer({
+    //   filterFn: (node) => {
+    //     // exclude files with the tag "explorerexclude"
+    //     return node.file?.frontmatter?.tags?.includes("explorerexclude") !== true
+    //   },
+    // })),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.MobileOnly(Component.Backlinks()),
+  ],
+}
+
+// components for pages that display lists of pages  (e.g. tags or folders)
+export const defaultListPageLayout: PageLayout = {
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  left: [
     Component.MobileOnly(Component.PageTitle()),
     Component.MobileOnly(Component.Darkmode()),
   ],
   right: [
     Component.DesktopOnly(Component.PageTitle()),
-    Component.DesktopOnly(Component.Darkmode()),
     Component.Search(),
-    Component.DesktopOnly(Component.Explorer()),
-    Component.DesktopOnly(Component.TableOfContents()),
- 
-    Component.MobileOnly(Component.Backlinks()),
-  ],
-}
- 
-// components for pages that display lists of pages  (e.g. tags or folders)
-export const defaultListPageLayout: PageLayout = {
-beforeBody: [
-    Component.ArticleTitle(),
-    Component.ContentMeta({showReadingTime: false, showComma: false,}),
-    Component.MobileOnly(Component.TableOfContents()),
-    
-  ],
-  left: [  
     Component.DesktopOnly(Component.Darkmode()),
-    Component.Graph(),
-    Component.Backlinks(),  
- 
-  ],
-  right: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Darkmode()),
-    Component.Search(),    
-    Component.DesktopOnly(Component.TableOfContents()),    
+    Component.DesktopOnly(Component.Explorer({
+      filterFn: (node) => {
+        // exclude files with the tag "explorerexclude"
+        return node.file?.frontmatter?.tags?.includes("explorerexclude") !== true
+      },
+    })),
   ],
 }
